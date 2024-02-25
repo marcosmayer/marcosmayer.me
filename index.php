fcc494538f67d112dc46c493b3bacabd3451aea2
Marcos Mayer <marcosmayer@me.com>
2024-02-25 10:38:54 -0300

    First commit

---
+++ index.php
@@ -0,0 +1,90 @@
+<!doctype html>
+
+<html lang="pt-br">
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+
+		<title>Marcos Mayer - Palestrante</title>
+		<meta name="description" content="Marcos Mayer acredita que cada pessoa tem um potencial infinito. Cada um de nós pode ser o seu próprio case. Marcos criou um programa de desenvolvimento humano chamado Seja o Seu C.A.S.E., com etapas importantes dessa transformação.">
+		<meta name="author" content="Marcos Mayer">
+
+		<meta property="og:title" content="Marcos Mayer - Palestrante">
+		<meta property="og:type" content="website">
+		<meta property="og:url" content="https://www.marcosmayer.me">
+		<meta property="og:description" content="Marcos Mayer acredita que cada pessoa tem um potencial infinito. Cada um de nós pode ser o seu próprio case. Marcos criou um programa de desenvolvimento humano chamado Seja o Seu C.A.S.E., com etapas importantes dessa transformação.">
+		<!-- <meta property="og:image" content="image.png"> -->
+
+        <meta name="robots" content="noindex">
+
+		<link rel="icon" href="/favicon.ico">
+		<link rel="icon" href="/favicon.svg" type="image/svg+xml">
+		<link rel="apple-touch-icon" href="/apple-touch-icon.png">
+
+		<link rel="stylesheet" href="styles/estilo.css?v=1.0">
+		<link rel="stylesheet" href="styles/normalize.css?v=8.0.1">
+
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Roboto+Condensed:ital,wght@0,100..900;1,100..900&family=Roboto+Flex:opsz,wght@8..144,100..1000&family=Roboto+Slab:wght@100..900&display=swap" rel="stylesheet">
+
+	</head>
+
+	<body>
+        <section class="hero">
+<!-- // * ÁREA DO HEADER COM O MENU MOBILE ==>  -->
+            <div class="header--mobile">
+                <div class="header__logo--mobile">
+                    <img src="images/marcos-mayer.svg" alt="Marcos Mayer - Palestrante">
+                </div>
+                <div class="menu-icon">
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                </div>
+                <nav class="mobile-menu">
+                    <ul>
+                        <li><a href="/marcosmayer/">Conheça o Marcos</a></li>
+                        <li><a href="/palestras/">Palestras</a></li>
+                        <li><a href="/case/">Seja o Seu C.A.S.E.</a></li>
+                        <li><a href="/blog/">Blog</a></li>
+                        <li><a href="/recursos/">Recursos gratuitos</a></li>
+                        <li><a href="/contato/">Contato</a></li>
+                    </ul>
+                </nav>
+            </div>
+<!-- // *  <== FIM DA ÁREA DO HEADER COM O MENU MOBILE  -->
+
+<!-- // * ÁREA DO HEADER COM O MENU DESKTOP ==>  -->
+            <div class="header">
+                <div class="header__logo">
+                    <img src="images/marcos-mayer.svg" alt="Marcos Mayer - Palestrante">
+                </div>
+                <nav class="header__nav">
+                    <ul class="header__menu">
+                        <li class="header__menu-item"><a class="link--menu" href="/marcos-mayer/" title="Conheça o Marcos">Conheça o Marcos</a></li>
+                        <li class="header__menu-item"><a class="link--menu" href="/palestras/" title="Palestras">Palestras</a></li>
+                        <li class="header__menu-item"><a class="link--menu" href="/seja-o-seu-case/" title="Seja o Seu C.A.S.E.">Seja o Seu C.A.S.E.</a></li>
+                        <li class="header__menu-item"><a class="link--menu" href="/blog/" title="Blog">Blog</a></li>
+                        <li class="header__menu-item"><a class="link--menu" href="/recursos/" title="Recursos gratuitos">Recursos gratuitos</a></li>
+                        <li class="header__menu-item"><a class="link--menu" href="/contato/" title="Contato">Contato</a></li>
+                    </ul>
+                </nav>
+                <div class="header__button">
+                    <a class="link--button" href="/reserva/">Reserve sua data</a>
+                </div>
+            </div>
+<!-- // * <== FIM DA ÁREA DO HEADER COM O MENU DESKTOP  -->
+            <div class="hero__content">
+                <div class="hero__program1">seja o seu</div>
+                <div class="hero__program2">C.A.S.E.</div>
+                <div class="hero__text">Por meio de um framework de desbloqueio da criatividade, cada pessoa pode se aproximar, infinitamente, do seu máximo potencial.</div>
+                <div class="hero__video">
+                    <img class="hero__video-icon" src="images/icon-play.svg" alt="Play"><span class="hero__video-text">Assista ao vídeo</span>
+                </div>
+            </div>
+        </section>
+        <script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
+        <script src="/scripts/scripts.js"></script>
+	</body>
+</html>
---
+++ scripts/scripts.js
@@ -0,0 +1,6 @@
+$(document).ready(function () {
+	$(".menu-icon").click(function () {
+		$(this).toggleClass("icon-active");
+		$(".mobile-menu").toggleClass("menu-active");
+	});
+});
